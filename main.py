dc7f6767de7af86852cc62b6a85923a66fdaddf2
Max Shishkov <maxshishkov92@gmail.com>
2026-01-07 18:59:42 -0600

    feat: add --verbose arg that displays metadata

--- main.py
+++ main.py
@@ -17,20 +17,23 @@ def main():
     
     parser = argparse.ArgumentParser(description="ChatBot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     
-    messages= [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages
     )
     
-    if response.usage_metadata:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        raise RuntimeError("usage metadata is None")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        if response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        else:
+            raise RuntimeError("usage metadata is None")
     
     print(response.text)
 
